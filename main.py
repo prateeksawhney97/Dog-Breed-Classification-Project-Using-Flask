758bf8b4e988549e9c0317ab7f54082b1e9241a4
prateeksawhney97 <prateek.sawhney97@gmail.com>
2020-05-15 18:43:01 +0530

    Updated Notebook

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import *  
 from extract_bottleneck_features import *
-
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import os
@@ -11,6 +14,35 @@ import glob
 #import tensorflow as tf
 from keras.models import load_model
 from random import randint
+
+ResNet50_model = load_model()
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))
 
 def get_correct_prenom(word, vowels):
     if word[0].lower() in vowels:
